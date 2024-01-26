6c9485f1a920247097983d513e76f1f8afdef14a
shivvang <shivanghariakar@email.com>
2024-01-26 12:26:43 +0530

    utils setting up 👍

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import dotenv from "dotenv";
 import connectDb from "./db/index.js";
+import { app } from "./app.js";
 //when mentioning path inside the config plz put path with utmost care
 dotenv.config();
-connectDb();
+connectDb()
+  .then(() => {
+    app.on("error", (err) => {
+      console.log("errr", err);
+      throw err;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log("mongo db connection failed !!", err);
+  });
 
 /*
 import express from "express";
---
+++ src/utils/ayncHandler.js
@@ -0,0 +1,26 @@
+const asyncHandler = (reqestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(reqestHandler(req, res, next)).catch((err) => {
+      next(err);
+    });
+  };
+};
+
+export { asyncHandler };
+
+// const asyncHandler =()=>{}
+// const asyncHandler =()=>{()=>{}}
+//fucntion is passed into another function
+
+//using try catch block
+
+// const asyncHandler =(fn)=>async(req,res,next)=>{
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
