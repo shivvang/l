97872338a21090f5bc88c34058479152fb3fabbd
shivvang <shivanghariakar@email.com>
2024-02-07 21:34:57 +0530

    some more user details  customization controller

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -266,7 +266,7 @@ const updateAccountDetails = asyncHandler(async (req, res) => {
     throw new ApiError(400, "All fields are required");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -298,7 +298,7 @@ const updateUserAvatar = asyncHandler(async (req, res) => {
     throw new ApiError(400, "error occured while uploading the file");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
@@ -325,7 +325,7 @@ const updateUserCoverImage = asyncHandler(async (req, res) => {
     throw new ApiError(400, "error occured while uploading the file");
   }
 
-  const user = User.findByIdAndUpdate(
+  const user = await User.findByIdAndUpdate(
     req.user?._id,
     {
       $set: {
